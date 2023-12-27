06becf5d397c76bd03567115308bdf1f6d0dd3e9
Tautvydas1997 <145842914+Tautvydas1997@users.noreply.github.com>
2023-12-27 22:22:33 +0200

    list<Studentas>

--- bibliotekos.h
+++ bibliotekos.h
@@ -7,6 +7,9 @@
 #include <algorithm>
 #include <sstream>
 #include <fstream>
+#include <chrono>
+#include <deque>
+#include <list>
 
 using std::cin;
 using std::cout;
@@ -19,12 +22,19 @@ using std::fixed;
 using std::left;
 using std::right;
 using std::string;
+using std::to_string;
 using std::vector;
 using std::ifstream;
+using std::ofstream;
 using std::getline;
 using std::stringstream;
 using std::exception;
+using std::deque;
+using std::list;
+using namespace std::chrono;
 
 #include "Studentas.h"
 #include "Funkcijos.h"
 #include "IsFailo.h"
+#include "FailuKurimas.h"
+#include "Skirstymas.h"
