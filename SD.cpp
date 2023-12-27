e53813b891037bdb40bad4c6b155d4c5a2fa3b51
Tautvydas1997 <145842914+Tautvydas1997@users.noreply.github.com>
2023-12-27 21:21:04 +0200

    Create SD.cpp
    Paskutine V0.1 modifikacija

---
+++ SD.cpp
@@ -0,0 +1,37 @@
+#include "bibliotekos.h"
+
+int main() {
+	int option = 0, kiek = 0;
+	string r, bud = "";
+	Studentas S;
+	vector<Studentas> Studentai;
+
+	while (option != 9) {
+		kiek = Studentai.size();
+		option = meniu();
+
+		switch (option) {
+		case 1:
+			// prideti studenta
+			cin >> S;
+			Studentai.push_back(S);
+			break;
+		case 2:
+			if (Studentai[0].getBudas() == false) {
+				bud = "Galutinis(Vidurkis)";
+			} else { bud = "Galutinis(Mediana)"; }
+			cout << setw(15) << left << "Pavarde" << setw(15) << left << "Vardas" << setw(15) << left << setprecision(2) << fixed << bud << endl;
+			cout << "------------------------------------------" << endl;
+			for (int j = 0; j < kiek; j++) {
+				cout << Studentai[j];
+			}
+			break;
+
+		case 3:
+			FailoSkaitymas();
+			cout << endl;
+			break;
+		}
+	}
+	return 0;
+}
