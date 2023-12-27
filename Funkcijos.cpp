643ac0c2431125db999ebcbcb67e61401be09438
Tautvydas1997 <145842914+Tautvydas1997@users.noreply.github.com>
2023-12-27 22:20:32 +0200

    Skirstymas i dvi grupes ir laiko skaiciavimas

--- Funkcijos.cpp
+++ Funkcijos.cpp
@@ -6,6 +6,8 @@ int meniu() {
 	cout << "1. Prideti studenta" << endl;
 	cout << "2. Duomenu atvaizdavimas (Vidurkis)" << endl;
 	cout << "3. Nuskaityti duomenis is failo ir rezultata pateikti i ekrana." << endl;
+	cout << "4. Sugeneruoti studentu faila su atsitiktiniais balais." << endl;
+	cout << "5. Studentu nuskaitymas is failo, grupavimas i vargsiukus ir kietekus, bei irasymas i du skirtingus failus." << endl;
 	cout << "------------" << endl;
 	cout << "9. Baigti" << endl;
 	cin >> op;
